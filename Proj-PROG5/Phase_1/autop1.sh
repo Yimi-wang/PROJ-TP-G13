5ff0987b840d2a640942e6ac81646067e6930286
Ephilope <75535648+Ephilope@users.noreply.github.com>
2022-01-12 22:12:45 +0100

    ajout script auto test Phase 1

---
+++ Proj-PROG5/Phase_1/autop1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+erreur () {
+    echo $* 1>&2
+    exit 1
+}
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+for e in "${@:2}"
+do
+    case $e in
+    1)  
+        printf "${GREEN}/////ETAPE 1/////  :${NC}\n"
+        ./Pharse1 -h tests/"$1".o
+        printf "\nreadelf :\n"
+        readelf tests/"$1".o -h | sed -n -E '/^  Machine|  Type|  Size|  Number of section|  Section header|  Class/p'
+        ;;
+    2)
+        printf "${GREEN}/////ETAPE 2/////  :${NC}\n"
+        ./Pharse1 -S tests/"$1".o
+        printf "\nreadelf :\n" 
+        readelf tests/"$1".o -S | sed -n -E '/^Section Headers|  \[/p'
+        ;; 
+    3)
+        printf "${GREEN}/////ETAPE 3/////  :${NC}\n"
+        ./Pharse1 -x .text tests/"$1".o
+        printf "\nreadelf :\n" 
+        readelf tests/"$1".o -x .text | sed -n -E '/^ NOTE/!p'
+        ;;
+    4)
+        printf "${GREEN}/////ETAPE 4/////  :${NC}\n"
+        ./Pharse1 -s tests/"$1".o
+        printf "\nreadelf :\n"
+        readelf tests/"$1".o -s
+        ;;
+    5)
+        printf "${GREEN}/////ETAPE 5/////  :${NC}\n"
+        ./Pharse1 -r tests/"$1".o
+        printf "\nreadelf :\n" 
+        readelf tests/"$1".o -r | awk '{print $1,"   "$3}'
+        ;;
+    *)
+        printf "mauvais num etape \n"
+        ;;
+    esac
+    printf "\n \n"
+done
