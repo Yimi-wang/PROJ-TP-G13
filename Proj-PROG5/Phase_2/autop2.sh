5acdfe938c45e66513dcf7784d887b63beccb9cd
Ephilope <75535648+Ephilope@users.noreply.github.com>
2022-01-13 16:00:58 +0100

    Modif shell example

--- Proj-PROG5/Phase_2/autop2.sh
+++ Proj-PROG5/Phase_2/autop2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./Phase2 --section-start .text=0x58 --section-start .data=0x1000 -o $1 tests/$1.o
+./Phase2 --section-start .text=0x58 --section-start .data=0x1000 -o $1 examples/$1.o
 printf "\n Disassembly of section .text \n"
 arm-none-eabi-objdump -Dx $1 | awk '/Disassembly of section .text:/{flag=1;next}/Disassembly/{flag=0}flag'
 printf "\n Disassembly of section .data \n"
